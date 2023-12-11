8ca632e231a42c1013c90ddb300d86f4588ae3d0
YashLGit <151878344+YashLGit@users.noreply.github.com>
2023-12-10 16:18:55 -0800

    Initial Commit
    Initial Commit

---
+++ WebScraper/WebScraper/WebScraper/Program.cs
@@ -0,0 +1,34 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
+
+namespace WebScraper
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            // Send get request to my LinkedIn Page
+            String url = "https://github.com/YashLGit";
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync(url).Result;
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            // Get the username
+            var nameElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='p-name vcard-fullname d-block overflow-hidden']");
+            var name = nameElement.InnerText.Trim();
+            Console.WriteLine("Name: " + name);
+
+            // Get the description
+            var descriptionElement = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='p-note user-profile-bio mb-3 js-user-profile-bio f4']");
+            var description = descriptionElement.InnerText.Trim();
+            Console.WriteLine("Description: " + description);
+
+            //Get the location
+            var locationElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='p-label']");
+            var location = locationElement.InnerText.Trim();
+            Console.WriteLine("Location: " + location);
+        }
+    }
+}
